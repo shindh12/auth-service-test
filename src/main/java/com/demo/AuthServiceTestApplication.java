779f4793f1663a8c2016259faebdf8daa9ae6614
donghashin <shindh159@gmail.com>
2018-12-06 22:29:13 +0900

    first commit

--- src/main/java/com/demo/AuthServiceTestApplication.java
+++ src/main/java/com/demo/AuthServiceTestApplication.java
@@ -1,12 +1,37 @@
 package com.demo;
 
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import com.demo.model.Role;
+import com.demo.model.User;
+import com.demo.model.Writer;
+import com.demo.repo.UserRepository;
 
 @SpringBootApplication
+//@EnableAuthorizationServer
 public class AuthServiceTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceTestApplication.class, args);
+	}
+
+//	@Autowired
+	public void authenticationManager(AuthenticationManagerBuilder builder, UserRepository repo) throws Exception {
+//		builder.userDetailsService(new UserDetailsService() {
+//			@Override
+//			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//				return new CustomUserDetails(repo.findByUsername(username));
+//			}
+//		});
+		if(repo.count() == 0) {
+			repo.save(new User("user","user",Arrays.asList(new Role("USER"), new Role("ACTUATOR"))));
+		}
+		builder.userDetailsService(username -> new CustomUserDetails(repo.findByUsername(username)));
 	}
 }
--- src/test/java/com/demo/AuthServiceTestApplicationTests.java
+++ src/test/java/com/demo/AuthServiceTestApplicationTests.java
@@ -1,16 +1,29 @@
 package com.demo;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.model.Role;
+import com.demo.model.Writer;
+import com.demo.repo.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthServiceTestApplicationTests {
-
+	
+	@Autowired
+	UserRepository repo;
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	@Test
+	public void Test() {
+		repo.save(new Writer("hey","hey",Arrays.asList(new Role("USER")),"타입"));
+	}
 }
